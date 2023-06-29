fd1b8d8800d79c7c9cb8bf05dffbbed68940ac2e
tzafff <christzaf99@gmail.com>
2023-06-29 19:56:35 +0300

    Example with Struct

---
+++ StructExample.sol
@@ -0,0 +1,38 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.15;
+
+contract PaymentReceived{
+
+    address public from;
+    uint public amount;
+
+    constructor(address _from, uint _amount){
+        from = _from;
+        amount = _amount;
+    }
+}
+
+contract Wallet {
+
+    PaymentReceived public payment;
+
+    function payContract() public payable {
+        payment = new PaymentReceived(msg.sender, msg.value);
+    }
+}
+
+//Simplify the abovd with structs
+contract Wallet2 {
+
+    struct PaymentReceivedStruct{
+        address from;
+        uint amount;
+    }
+
+    PaymentReceivedStruct public payment;
+
+    function payContract() public payable {
+        payment = PaymentReceivedStruct(msg.sender, msg.value);
+    }
+}
