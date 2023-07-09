84c12b4a4096a1c43e76f1614937913b04dbc418
tzafff <christzaf99@gmail.com>
2023-07-09 16:04:51 +0300

    Using Console.Log Inside Solidity Using Ganache

--- truffle/contracts/Spacebear.sol
+++ truffle/contracts/Spacebear.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract CheapBoredApeYachtClub is ERC721, ERC721URIStorage, Ownable {
+import "@ganache/console.log/console.sol";
+contract CheapBoredApeYachtClub is ERC721, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
@@ -17,32 +17,39 @@ contract CheapBoredApeYachtClub is ERC721, ERC721URIStorage, Ownable {
         return "ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/";
     }
 
+    function byeToken() public payable{
+        uint256 tokenId = _tokenIdCounter.current();
+        console.log("got here",tokenId,msg.value);
+        require(msg.value == tokenId * 0.1 ether, "Not enought funds");
+        _tokenIdCounter.increment();
+        _safeMint(msg.sender, tokenId);
+    }
+
     function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
     }
 
     // The following functions are overrides required by Solidity.
 
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    function _burn(uint256 tokenId) internal override(ERC721) {
         super._burn(tokenId);
     }
 
     function tokenURI(uint256 tokenId)
         public
-        view
-        override(ERC721, ERC721URIStorage)
+        pure
+        override(ERC721)
         returns (string memory)
     {
-        return super.tokenURI(tokenId);
+        return string(abi.encodePacked(_baseURI(),"bayc_",(tokenId+1),".json"));
     }
 
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721URIStorage)
+        override(ERC721)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
